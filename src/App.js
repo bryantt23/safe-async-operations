7887392df86c8452812b64cf0b6ddfa7e7b96e6d
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-06-26 18:45:07 -0700

    Add button and state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState}from 'react'
+import { useState } from 'react';
 function App() {
-  const [isMounted, setIsMounted]=useState(true)
+  const [isMounted, setIsMounted] = useState(true);
+  const [inputNumber, setInputNumber] = useState(null);
   return (
-    <div className="App">
-    {JSON.stringify(isMounted)}
-
-          <input
-            name="isGoing"
-            type="checkbox"
-            checked={isMounted}
-            onChange={()=>{setIsMounted(!isMounted)}} />
-            Mount the search bar
-
+    <div className='App'>
+      {JSON.stringify(isMounted)}
+      {JSON.stringify(inputNumber)}
+      <input
+        name='isGoing'
+        type='checkbox'
+        checked={isMounted}
+        onChange={() => {
+          setIsMounted(!isMounted);
+        }}
+      />
+      Mount the search bar Fetch
+      {isMounted && (
+        <div>
+          <label>
+            {' '}
+            <input
+              name='numberOfGuests'
+              type='number'
+              value={inputNumber}
+              placeholder=' Pick a number!'
+              onChange={e => setInputNumber(e.target.value)}
+            />
+          </label>
+          <button disabled={inputNumber === null}>Fetch</button>
+          <p>Which Rick and Morty Character?</p>
+        </div>
+      )}
     </div>
   );
 }
