aed1853cfdffe97770459beffddc858f0496fefc
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-06-26 18:35:06 -0700

    Set up checkbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState}from 'react'
 function App() {
+  const [isMounted, setIsMounted]=useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {JSON.stringify(isMounted)}
+
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={isMounted}
+            onChange={()=>{setIsMounted(!isMounted)}} />
+            Mount the search bar
+
     </div>
   );
 }
